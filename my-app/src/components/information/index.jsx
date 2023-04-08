60f1f07a11308bd717869e93cbb03a35b66a1395
Saten170 <satenik.2003karapetyan@mail.ru>
2023-04-08 00:17:19 -0700

    function

--- my-app/src/components/information/index.jsx
+++ my-app/src/components/information/index.jsx
@@ -1,7 +1,9 @@
+import React, { useState} from "react";
 import './style.scss'
 
 const Information=({title, description}) =>{
-    return <div className={`P-box}`}>
+    return <div className={`P-box
+}`}>
         <h3>{title} </h3>
         {description ? <p>{description}</p> : null}
         <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad, odit.</span> 
